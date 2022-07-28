224b54ed453c10cd243ebfd29cf4a411a4965b28
Realodix <budi03@gmail.com>
2022-07-29 03:12:30 +0700

    Realodix: Remove binary_operator_spaces

--- src/Rules/Realodix.php
+++ src/Rules/Realodix.php
@@ -20,7 +20,6 @@ final class Realodix extends AbstractRules
             'Laravel/laravel_phpdoc_alignment' => false,
             'Laravel/laravel_phpdoc_order' => false,
             'Laravel/laravel_phpdoc_separation' => false,
-            'binary_operator_spaces' => ['operators' => ['=>' => 'single_space']],
 
             /*
              * Addition
