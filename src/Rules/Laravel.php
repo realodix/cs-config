07c39614c1c0d1e83658009e351d88af2445b8ae
Realodix <budi03@gmail.com>
2022-07-28 13:19:19 +0700

    Laravel: Update method_argument_space rule in Laravel preset
    https://github.com/laravel/pint/commit/ee3d146053ebe863a5cbb85c8d3371e184b7d0bc

--- src/Rules/Laravel.php
+++ src/Rules/Laravel.php
@@ -77,9 +77,7 @@ final class Laravel extends AbstractRules
             'lowercase_static_reference'   => true,
             'magic_constant_casing'        => true,
             'magic_method_casing'          => true,
-            'method_argument_space'        => [
-                'on_multiline' => 'ignore',
-            ],
+            'method_argument_space'        => true,
             'multiline_whitespace_before_semicolons' => [
                 'strategy' => 'no_multi_line',
             ],
