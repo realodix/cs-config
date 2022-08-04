355405151267fea37f869ce92d00cdfc1bec27e5
Realodix <budi03@gmail.com>
2022-08-04 23:17:27 +0700

    Remove unused function

--- tests/FinderTest.php
+++ tests/FinderTest.php
@@ -9,28 +9,6 @@ use Realodix\CsConfig\Finder;
 class FinderTest extends TestCase
 {
     /**
-     * It only includes existing paths
-     *
-     * @test
-     */
-    public function itOnlyIncludesExistingPaths(): void
-    {
-        $testDirs = [
-            realpath(__DIR__.'/../src'),
-            realpath(__DIR__.'/../tests'),
-            realpath(__DIR__.'/../missing-dir-1'),
-            realpath(__DIR__.'/../missing-dir-2'),
-        ];
-        $existingPaths = Finder::onlyExistingPaths($testDirs);
-
-        $this->assertCount(2, $existingPaths);
-        $this->assertEquals([
-            realpath(__DIR__.'/../src'),
-            realpath(__DIR__.'/../tests'),
-        ], $existingPaths);
-    }
-
-    /**
      * It returns a PHP CS Fixer finder object
      *
      * @test
